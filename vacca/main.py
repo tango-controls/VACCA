237825b424d77023b79175425bf4b222704d5328
Sergio Rubio Manrique <srubio@cells.es>
2016-09-13 17:08:48 +0200

    Solved bug with paths and env variables

--- vacca/main.py
+++ vacca/main.py
@@ -65,16 +65,17 @@ def remove_last_config(filename):
 
 folder = os.getenv('HOME')+'/.config/VACCA/'
 if '--clean' in args:
+  print('Removing last vacca configs (%s/*.ini)'%folder)
   os.remove(folder+'*.ini')
   if len(args)==1: sys.exit(0)
 elif '--reset' in args:
   inits = [a for a in os.walk(f).next()[2] if a.endswith('.ini')]
+  print('Removing last vacca configs (%s)'%inits)
   [remove_last_config(folder+filename) for filename in inits]
   if len(args)==1: sys.exit(0)
   
 ###############################################################################
-
-print '-'*80
+# Delayed imports
 
 import taurus
 from taurus.core.util import argparse
@@ -84,6 +85,11 @@ from taurus.qt.qtgui.taurusgui import TaurusGui
 import vacca.utils as vu
 import vacca 
 
+###############################################################################
+
+print('-'*80)
+print("In vacca.main(%s) ..."%args)
+
 options = [a for a in args if a.startswith('-')]
 values = [a for a in args if '=' in a]
 files = [a for a in args if a not in options+values] or [os.getenv('VACCA_CONFIG')]
@@ -92,14 +98,15 @@ configs = vu.get_config_properties() or vu.create_config_properties()
 if not files or not files[0]: files = [configs.keys()[0]]
 
 dirname = os.getenv('VACCA_DIR') or ''
+config = os.getenv('VACCA_CONFIG') or ''
 
 if files[0] in configs:
     print('Loading %s'%files[0])
     data = vu.get_config_properties(files[0])
-    config = data.get('VACCA_CONFIG',files[0])
-    dirname = data.get('VACCA_DIR',dirname)
+    config = config or data.get('VACCA_CONFIG',files[0])
+    dirname = dirname or data.get('VACCA_DIR',dirname)
 else: 
-    config = files[0]
+    config = config or files[0]
     
 if os.path.isfile(config):
   config = os.path.abspath(config)
@@ -119,13 +126,11 @@ dirname = dirname or os.path.dirname(config) or \
 vu.VACCA_DIR = os.environ['VACCA_DIR'] = dirname
 vu.VACCA_CONFIG = os.environ['VACCA_CONFIG'] = config
 
-print('Vacca Environment variables:')
-print(dict((k,v) for k,v in os.environ.items() if 'VACCA' in k))
-print '-'*80
-
+print('Vacca Environment variables (vacca.main):')
+print('\n'.join(map(str,(t for t in os.environ.items() if 'VACCA' in t[0]))))
   
 ### MAIN CODE FOR PANELS GENERATION IS IN vacca.config SUBMODULE
-
+print '-'*80
 confname = 'vaccagui'
 app = TaurusApplication()
 gui = TaurusGui(None, confname=confname)
